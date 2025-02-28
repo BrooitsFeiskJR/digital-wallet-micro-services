aa0764fa86da56e7496d298046fb9e6ae28c96f3
BrooitsFeiskJR <luizantonio0125@gmail.com>
2025-02-28 12:00:35 -0300

    Test: add unit tests for user constructor and validation logic

---
+++ user-service/entities/user_entity_test.go
@@ -0,0 +1,82 @@
+package entities
+
+import (
+	"testing"
+
+	"github.com/BrooitsFeiskJR/digital-wallet-user-service/dto"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUserConstructor(t *testing.T) {
+	dto := dto.CreateUserDTO{
+		Name:        "example",
+		Email:       "example@example.com",
+		Password:    "password",
+		PhoneNumber: "08123456789",
+		CPF:         "12345678909",
+	}
+	user, err := NewUser(dto)
+	if err != nil {
+		t.Errorf("Error creating user: %v", err)
+	}
+	if user == nil {
+		t.Errorf("Expected user, got nil")
+		return
+	}
+	assert.Equal(t, dto.Name, user.Name)
+	assert.Equal(t, dto.Email, user.Email)
+	assert.Equal(t, dto.PhoneNumber, user.PhoneNumber)
+	assert.Equal(t, dto.CPF, user.CPF)
+}
+
+func TestUserConstructorInvalidPhoneNumber(t *testing.T) {
+	dto := dto.CreateUserDTO{
+		Name:        "example",
+		Email:       "example@example.com",
+		Password:    "password",
+		PhoneNumber: "invalid-phone-number",
+		CPF:         "12345678909",
+	}
+	user, err := NewUser(dto)
+	assert.NotNil(t, err)
+	assert.Nil(t, user)
+}
+
+func TestUserConstructorInvalidEmail(t *testing.T) {
+	dto := dto.CreateUserDTO{
+		Name:        "example",
+		Email:       "invalid-email",
+		Password:    "password",
+		PhoneNumber: "1234567890",
+		CPF:         "12345678909",
+	}
+	user, err := NewUser(dto)
+	assert.NotNil(t, err)
+	assert.Nil(t, user)
+}
+
+func TestUserConstructorInvalidCPF(t *testing.T) {
+	dto := dto.CreateUserDTO{
+		Name:        "example",
+		Email:       "example@example.com",
+		Password:    "password",
+		PhoneNumber: "1234567890",
+		CPF:         "invalid-cpf",
+	}
+	user, err := NewUser(dto)
+	assert.NotNil(t, err)
+	assert.Nil(t, user)
+}
+
+func TestUserConstructorMissingFields(t *testing.T) {
+	dto := dto.CreateUserDTO{
+		Name:        "",
+		Email:       "",
+		Password:    "",
+		PhoneNumber: "",
+		CPF:         "",
+	}
+	user, err := NewUser(dto)
+	assert.NotNil(t, err)
+	assert.Nil(t, user)
+}
