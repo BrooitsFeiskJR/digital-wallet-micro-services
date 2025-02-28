c7c43e5253763dbc4ca243a52696e26aff6e5d1d
BrooitsFeiskJR <luizantonio0125@gmail.com>
2025-02-28 18:30:50 -0300

    Fix: add unit tests for password and confirm password validation

--- user-service/domain/validation/validate_password_and_confirm_password_test.go
+++ user-service/domain/validation/validate_password_and_confirm_password_test.go
@@ -1,1 +1,21 @@
 package validation
+
+import "testing"
+
+func TestValidationPasswordAndConfirmPassword(t *testing.T) {
+	password := "password"
+	confirmPassword := "password"
+	result := ValidatePasswordAndConfirmPassword(password, confirmPassword)
+	if !result {
+		t.Errorf("Expected %v, got %v", true, result)
+	}
+}
+
+func TestValidationPasswordAndConfirmPasswordDifferent(t *testing.T) {
+	password := "password"
+	confirmPassword := "password123"
+	result := ValidatePasswordAndConfirmPassword(password, confirmPassword)
+	if result {
+		t.Errorf("Expected %v, got %v", false, result)
+	}
+}
