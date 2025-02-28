f8f30de7acce8bf5c0b4f52b5c28b454822e41e9
BrooitsFeiskJR <luizantonio0125@gmail.com>
2025-02-28 15:06:51 -0300

    Feat: add db tags to UserDTO fields for database mapping

--- user-service/domain/dto/user_dto.go
+++ user-service/domain/dto/user_dto.go
@@ -6,7 +6,7 @@ type UserDTO struct {
 	ID          uuid.UUID `json:"id"`
 	Name        string    `json:"name"`
 	Email       string    `json:"email"`
-	PhoneNumber string    `json:"phone_number"`
+	PhoneNumber string    `json:"phone_number" db:"phone_number"`
 	CPF         string    `json:"cpf"`
-	CreatedAt   string    `json:"created_at"`
+	CreatedAt   string    `json:"created_at" db:"created_at"`
 }
