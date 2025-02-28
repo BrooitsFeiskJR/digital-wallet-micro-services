420ec629692efc9a1e366cb697a515fc90124340
BrooitsFeiskJR <luizantonio0125@gmail.com>
2025-02-28 14:51:18 -0300

    Feat: add created_at and updated_at fields to user entity and enhance NewUser function validation

--- user-service/domain/entities/user_entity.go
+++ user-service/domain/entities/user_entity.go
@@ -2,6 +2,7 @@ package entities
 
 import (
 	"errors"
+	"time"
 
 	"github.com/BrooitsFeiskJR/digital-wallet-user-service/domain/dto"
 	"github.com/BrooitsFeiskJR/digital-wallet-user-service/domain/validation"
@@ -15,15 +16,21 @@ type User struct {
 	Password    string    `json:"password" db:"password"`
 	PhoneNumber string    `json:"phone_number" db:"phone_number"`
 	CPF         string    `json:"cpf" db:"cpf"`
+	CreatedAt   string    `json:"created_at" db:"created_at"`
+	UpdateAt    string    `json:"updated_at" db:"updated_at"`
 }
 
-func NewUser(dto dto.CreateUserDTO) (*User, error) {
+func NewUser(dto *dto.CreateUserDTO) (*User, error) {
 	if err := validation.ValidateUserRequiredFields(dto.Name, dto.Email, dto.Password, dto.PhoneNumber, dto.CPF); err != nil {
 		return nil, err
 	}
 	result := validation.ValidateEmailField(dto.Email)
 	if !result {
 		return nil, errors.New("email is invalid")
+	}
+	result = validation.ValidatePasswordField(dto.Password)
+	if !result {
+		return nil, errors.New("password length must be greater than 8")
 	}
 	hash, err := validation.HashPassword(dto.Password)
 	if err != nil {
@@ -44,5 +51,7 @@ func NewUser(dto dto.CreateUserDTO) (*User, error) {
 		Password:    hash,
 		PhoneNumber: dto.PhoneNumber,
 		CPF:         dto.CPF,
+		CreatedAt:   time.Now().Format(time.RFC3339),
+		UpdateAt:    time.Now().Format(time.RFC3339),
 	}, nil
 }
