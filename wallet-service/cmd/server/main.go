1be71544b299ba16b7e38f0d06c1d990b067e0ea
BrooitsFeiskJR <luizantonio0125@gmail.com>
2025-03-05 16:44:26 -0300

    Feat: integrate MongoDB connection in main function with error handling

--- wallet-service/cmd/server/main.go
+++ wallet-service/cmd/server/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/BrooitsFeiskJR/digital-wallet-wallet-service/infra/db"
+)
 
 func main() {
 	fmt.Println("Hello, Wallet Service!")
+	uri, err := db.GetMongoURI()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to get mongo uri: %v\n", err)
+	}
+	_, err = db.ConnectMongoDB(uri)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to connect to mongo: %v\n", err)
+	}
 }
