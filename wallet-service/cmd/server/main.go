00b90c42077aaf05ec35ec3650986bd088e9d675
BrooitsFeiskJR <luizantonio0125@gmail.com>
2025-03-06 13:12:55 -0300

    Feat: enhance main server logic to handle graceful shutdown and MongoDB connection

--- wallet-service/cmd/server/main.go
+++ wallet-service/cmd/server/main.go
@@ -1,20 +1,48 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"log"
 	"os"
+	"os/signal"
+	"syscall"
 
+	"github.com/BrooitsFeiskJR/digital-wallet-wallet-service/api/handlers"
+	"github.com/BrooitsFeiskJR/digital-wallet-wallet-service/domain/services"
 	"github.com/BrooitsFeiskJR/digital-wallet-wallet-service/infra/db"
+	"github.com/BrooitsFeiskJR/digital-wallet-wallet-service/infra/repositories"
 )
 
 func main() {
-	fmt.Println("Hello, Wallet Service!")
-	uri, err := db.GetMongoURI()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "failed to get mongo uri: %v\n", err)
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer cancel()
+	mongoURI := os.Getenv("MONGO_URI")
+	if mongoURI == "" {
+		log.Fatal("MONGO_URI environment variable is required")
 	}
-	_, err = db.ConnectMongoDB(uri)
+
+	mongoClient, err := db.ConnectMongoDB(mongoURI)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "failed to connect to mongo: %v\n", err)
+		log.Fatalf("Failed to connect to MongoDB: %v", err)
 	}
+
+	walletRepo := repositories.NewWalletRepository(mongoClient)
+
+	// Initialize services
+	walletService := services.NewWalletService(walletRepo, ctx)
+
+	// Initialize handlers
+	walletHandler := handlers.NewWalletHandler(walletService)
+
+	go walletHandler.NewWalletForNewUserHandler(ctx)
+
+	blockUntilShutdown()
+}
+
+func blockUntilShutdown() {
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
+
+	<-quit
+
 }
