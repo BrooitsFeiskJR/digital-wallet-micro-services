06182cfa6587bddbc5dbe67e7fcb38beac723448
BrooitsFeiskJR <luizantonio0125@gmail.com>
2025-03-06 10:52:56 -0300

    Feat: update WalletDTO to use uuid for UserID and adjust BSON field names

--- wallet-service/domain/dto/wallet_dto.go
+++ wallet-service/domain/dto/wallet_dto.go
@@ -3,12 +3,13 @@ package dto
 import (
 	"time"
 
+	"github.com/google/uuid"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type WalletDTO struct {
-	ID       primitive.ObjectID `bson:"id,omitempty" json:"id"`
-	UserID   primitive.ObjectID `bson:"user_id,omitempty" json:"user_id"`
+	ID       primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	UserID   uuid.UUID          `bson:"user_id" json:"user_id"`
 	Balance  float64            `bson:"balance,omitempty" json:"balance"`
 	CreateAt time.Time          `bson:"create_at,omitempty" json:"create_at"`
 	UpdateAt time.Time          `bson:"update_at,omitempty" json:"update_at"`
