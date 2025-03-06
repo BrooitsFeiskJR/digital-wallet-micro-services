ceda5be46c1969b708230400ce69a6f29203f5bd
BrooitsFeiskJR <luizantonio0125@gmail.com>
2025-03-06 13:13:02 -0300

    Feat: update WalletRepository interface to include context in GetWalletByUserID and modify Deposit method signature

--- wallet-service/domain/repositories/wallet_repository_interface.go
+++ wallet-service/domain/repositories/wallet_repository_interface.go
@@ -1,13 +1,14 @@
 package repositories
 
 import (
+	"context"
+
 	"github.com/BrooitsFeiskJR/digital-wallet-wallet-service/domain/dto"
-	"github.com/BrooitsFeiskJR/digital-wallet-wallet-service/domain/entities"
 )
 
 type WalletRepository interface {
 	SaveWallet(wallet *dto.CreateWalletDTO) (any, error)
-	GetWalletByUserID(userID string) (*dto.WalletDTO, error)
-	Deposit(wallet *dto.WalletDTO, amount float64) (any, error)
-	Withdraw(wallet *entities.Wallet, amount float64) (any, error)
+	GetWalletByUserID(ctx context.Context, userID string) (*dto.WalletDTO, error)
+	Deposit(wallet *dto.WalletDTO, amount float64) error
+	Withdraw(wallet *dto.WalletDTO, amount float64) (any, error)
 }
