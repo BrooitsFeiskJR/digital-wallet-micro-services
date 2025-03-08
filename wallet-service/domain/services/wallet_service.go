ee6a25333b0b9fecc09838bd87eb6af88f68fad6
BrooitsFeiskJR <luizantonio0125@gmail.com>
2025-03-08 13:30:45 -0300

    Feat: implement deposit and withdrawal methods in wallet service

--- wallet-service/domain/services/wallet_service.go
+++ wallet-service/domain/services/wallet_service.go
@@ -31,3 +31,29 @@ func (ws *WalletService) GetWalletByUserID(userID string) (*dto.WalletDTO, error
 	}
 	return ws.repository.GetWalletByUserID(ws.ctx, userID)
 }
+
+func (ws *WalletService) DepositToUserWallet(deposit *dto.DepostiWalletDTO) error {
+	if deposit.Amount <= 0 {
+		return errors.New("invalid deposit amount")
+	}
+	if deposit.UserID == "" {
+		return errors.New("empty user id")
+	}
+	if err := ws.repository.Deposit(deposit); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (ws *WalletService) WithdrawUserWallet(withdraw *dto.WithdrawWalletDTO) error {
+	if withdraw.Amount <= 0 {
+		return errors.New("invalid withdraw amount")
+	}
+	if withdraw.UserID == "" {
+		return errors.New("empty user id")
+	}
+	if err := ws.repository.Withdraw(withdraw); err != nil {
+		return err
+	}
+	return nil
+}
