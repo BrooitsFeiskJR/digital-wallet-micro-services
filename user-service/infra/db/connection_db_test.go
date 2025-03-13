46fb205038f5f1cd5f2a64d354d5b2c99d4432b8
BrooitsFeiskJR <luizantonio0125@gmail.com>
2025-03-13 14:22:42 -0300

    Feat: add tests for GetDBConnection function to handle empty connection string and multiple calls

--- user-service/infra/db/connection_db_test.go
+++ user-service/infra/db/connection_db_test.go
@@ -20,3 +20,28 @@ func TestGetDBConnection(t *testing.T) {
 	assert.Nil(t, err)
 	assert.NotNil(t, db)
 }
+
+func TestGetDBConnectionWithEmptyConnString(t *testing.T) {
+	validConnString := ""
+	db, err := GetDBConnection(validConnString)
+	assert.Nil(t, db)
+	assert.NotNil(t, err)
+	assert.Equal(t, "connection string is empty", err.Error())
+}
+
+func TestGetDBConnectionWithMultipleCalls(t *testing.T) {
+	err := godotenv.Load("../../.env")
+	if err != nil {
+		t.Fatalf("Error loading .env file")
+	}
+	validConnString := os.Getenv("DATABASE_URL")
+	db1, err1 := GetDBConnection(validConnString)
+	assert.Nil(t, err1)
+	assert.NotNil(t, db1)
+
+	db2, err2 := GetDBConnection(validConnString)
+	assert.Nil(t, err2)
+	assert.NotNil(t, db2)
+
+	assert.Equal(t, db1, db2)
+}
