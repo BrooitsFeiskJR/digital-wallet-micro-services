fd039702cf69e03b799fd2210ad58d1001bfe94f
BrooitsFeiskJR <luizantonio0125@gmail.com>
2025-03-14 16:36:44 -0300

    Feat: add public key parsing and implement token verification function

--- user-service/infra/auth/token.go
+++ user-service/infra/auth/token.go
@@ -13,17 +13,19 @@ import (
 
 var (
 	privateKey *rsa.PrivateKey
+	publicKey  *rsa.PublicKey
 )
 
 func init() {
 	privateKeyPEM := strings.ReplaceAll(os.Getenv("JWT_PRIVATE_KEY"), "\\n", "\n")
+	publicPEM := strings.ReplaceAll(os.Getenv("JWT_PUBLIC_KEY"), "\\n", "\n")
 
 	var err error
 	privateKey, err = jwt.ParseRSAPrivateKeyFromPEM([]byte(privateKeyPEM))
 	if err != nil {
 		fmt.Printf("Error parsing private key: %v\n", err)
 	}
-
+	publicKey, err = jwt.ParseRSAPublicKeyFromPEM([]byte(publicPEM))
 	if err != nil {
 		fmt.Printf("Error parsing public key: %v\n", err)
 	}
@@ -62,4 +64,29 @@ func CreateRefreshAcessToken(id uuid.UUID, name, email string) (string, error) {
 		return "", err
 	}
 	return refreshToken, nil
+}
+
+func VerifyToken(tokenString string, claimsParam jwt.Claims) error {
+	token, err := jwt.ParseWithClaims(tokenString, claimsParam, func(token *jwt.Token) (any, error) {
+		return publicKey, nil
+	})
+
+	if err != nil {
+		return err
+	}
+
+	if !token.Valid {
+		return fmt.Errorf("invalid token")
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok {
+		return fmt.Errorf("invalid token claims")
+	}
+
+	expirationTime := time.Unix(int64(claims["exp"].(float64)), 0)
+	if time.Now().UTC().After(expirationTime) {
+		return fmt.Errorf("token has expired")
+	}
+	return nil
 }
